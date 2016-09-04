1dad88b12effb93a17380cbd8869c188f9419ddb
Ana Gabriela Garcia <gaby.garciaparamo07@gmail.com>
2016-09-04 11:59:54 -0500

    Add of Complex Numbers

---
+++ src/Complex.java
@@ -0,0 +1,45 @@
+
+class Complex {
+	private double Real;
+	private double Img;
+	
+	public Complex(){
+		Real=4.0;
+		Img=6.0;
+	}
+	
+	public Complex(double a, double b){
+		this.Real = a;
+		this.Img = b;
+	}
+	
+	public Complex AddComplex(Complex n1, Complex n2){
+		Complex Result = new Complex();
+		
+		Result.Real = n1.Real + n2.Real;
+		Result.Img = n1.Img + n2.Img;
+		
+		return Result;
+	}
+	
+	public void Show(){
+		System.out.println(Real+ "+(" +Img +"i)");
+	}
+	
+	public void SetComplex(double a, double b){
+		this.Real = a;
+		this.Img = b;
+	}
+	
+	public void SetReal(double r){
+		this.Real = r;
+	}
+	
+	public void SetImg(double i){
+		this.Img = i;
+	}
+	
+	public double GetReal(){
+		return Real;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Complex n1 = new Complex(3.0, 2.0);
+		Complex n2 = new Complex(2.0, 7.0);
+		Complex Result = new Complex();
+		
+		Result=n1.AddComplex(n1, n2);
+		System.out.println("El resultado de la suma es: ");
+		Result.Show();
+		
+	}
+
+}
